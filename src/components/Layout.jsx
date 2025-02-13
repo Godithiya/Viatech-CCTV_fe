347aafc6f8c5b7032076bad93923bd22252999f8
Godithiya <anggaadithiya@gmail.com>
2025-02-14 00:42:30 +0700

    component display CCTV

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Header from './Header'
+import ButtonAudioAction from './ButtonAudioAction'
+import { Outlet } from 'react-router-dom'
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className='w-screen h-screen flex flex-col bg-[#F1F1F1]'>
       <div className='w-full h-24 bg-[#191919]'>
         <Header/>
       </div>
       <div className="w-screen flex flex-1 flex-col px-32 py-11">
-        {children}
+        <Outlet/>
       </div>
     </div>
   )
